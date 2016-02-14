0a70b075a7a8e9e9a8e4cfbb976ca98767545a1a
stormysunshine <jenniferchaiken@yahoo.com>
2016-02-14 01:40:17 +0000

    add image uploader

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# to avoid error while running rails console
+require 'carrierwave/orm/activerecord'
