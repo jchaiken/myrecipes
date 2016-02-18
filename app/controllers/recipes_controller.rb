05c1a95bb58a26b78d5da4fa0b9f89c0c1d2c826
stormysunshine <jenniferchaiken@yahoo.com>
2016-02-18 17:57:43 +0000

    add chefs listing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -64,8 +64,7 @@ class RecipesController < ApplicationController
     def require_same_user
       if current_user != @recipe.chef
         flash[:danger] = "You can only edit your own recipes"
-        redirect_to recipes_path
+        redirect_to :back
       end
     end
-  
 end
